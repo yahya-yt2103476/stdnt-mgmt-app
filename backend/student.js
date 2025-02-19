c06fcd9c7164fe09af70b370fc525d3d2ed792db
ayham-ad2313830 <ad2313830@qu.edu.qa>
2025-02-20 01:16:44 +0400

    Learning path page

---
+++ backend/student.js
@@ -0,0 +1,17 @@
+class Student{
+    name;
+    major;
+    gpa;
+    id;
+    constructor(id,name,gpa,major)
+    {
+        this.name=name;
+        this.gpa=gpa;
+        this.major=major;
+        this.id=id;
+    }
+
+    getStudentCourses()
+    {
+    }
+}
