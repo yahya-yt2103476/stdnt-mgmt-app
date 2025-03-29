844f86466080ff794b324cedc85c949cac365b5f
Ayoub-aa2111262 <aa2111262@qu.edu.qa>
2025-03-29 18:50:25 +0300

    sections

--- frontend/pages/student-view/stdnt-mp/index.js
+++ frontend/pages/student-view/stdnt-mp/index.js
@@ -46,22 +46,30 @@ async function loadsections(courseid) {
     })
     SectionsContainer.innerHTML = ``
 
-    const sections = data.find(s => s.courseId == courseid)
-    const sectionsArray = Array.isArray(sections) ? sections : [sections]; 
+    const sections = data.filter(s => s.courseId == courseid)
+    const sectionsArray = Array.isArray(sections) ? sections : [sections];
+    console.log("testing");
+    sectionsArray.forEach(s=>console.log(s)
+    )
+    if (!sectionsArray) {
+        SectionsContainer +=`
+        <p>No available Sections for this course...</p>
+        `
+    } else {
+        
+    }
     sectionsArray.forEach((sec) => {
         SectionsContainer.innerHTML += `
-        <div class="container">
-            <div class="header">${sec.id} (${sec.courseId})</div>
-            <div class="courseDetails">
-                <b>Course Details:</b>
-                <p>Title: ${sec.courseId}</p>
-            </div>
-            <div class="description">
-                <p>${course.Description}</p>
-            </div>
-            <div class="btn-container"><button onclick="loadsections('${course.id}')">View Sections</button></div>
-
-        </div>
+        <div class="sectioncontainer">
+        <p class="section-id">Section ID: ${sec.id}</p>
+        <p>Instructor: ${sec.instructorName}</p>
+        <p>Semester: ${sec.semester}</p>
+        <p>capacity: ${sec.capacity}</p>
+        <p>remaining seats: 18</p>
+        <p>Time: ${sec.Time}</p>
+        <p>Days: ${sec.Days}</p>
+        <button id="Register-sections">Register</button>
+    </div>
         `
     })
 
