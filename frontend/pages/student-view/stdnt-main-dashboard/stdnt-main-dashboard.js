f6774e0b00770c5184f6286f24760ec41de2d7ce
Ayoub-aa2111262 <aa2111262@qu.edu.qa>
2025-04-10 01:26:50 +0300

    half working registration function

--- frontend/pages/student-view/stdnt-main-dashboard/stdnt-main-dashboard.js
+++ frontend/pages/student-view/stdnt-main-dashboard/stdnt-main-dashboard.js
@@ -1,12 +1,23 @@
 import { LogoutCurrentUser } from "../../../services/logout.js";
+import { fetchStudentById } from "../../../services/student-service.js";
+
+
+
 const coursesRegistrationCard = document.getElementById("coursesRegistration");
-
-//logout functionality
-const logoutbtn = document.querySelector("#log-out-btn")
-logoutbtn.addEventListener("click", LogoutCurrentUser);
-
-
 const learningPathCard = document.getElementById("learningPath");
+const logoutbtn = document.querySelector("#log-out-btn")
+const WelcomeMessage = document.querySelector("#WelcomeMsg");
+
+
+const student = await fetchStudentById(sessionStorage.getItem("authenticated_user_id"));
+console.log(student);
+WelcomeMessage.innerHTML = `Welcome back ${student.name}!`;
+
+
+
+
+
+logoutbtn.addEventListener("click", LogoutCurrentUser);
 
 coursesRegistrationCard.addEventListener("click", function () {
   window.location.href = '../stdnt-registration/stdnt-registration.html';
@@ -16,10 +27,3 @@ learningPathCard.addEventListener("click", function () {
   window.location.href = "../learning-path/learning-path-page.html";
 });
 
-
-
-function handleDelete() {
-  sessionStorage.removeItem("authenticated_user_id")
-  window.location.href = "http://127.0.0.1:5500/frontend/pages/login/login_page.html";
-
-}
--- frontend/services/section-service.js
+++ frontend/services/section-service.js
@@ -18,6 +18,17 @@ async function createSection(data) {
   return await saveDataToApi('/sections', newSection);
 }
 
+async function addStudentToSection(sectionId, studentId) {
+  const section = await fetchSectionById(sectionId);
+  if (section) {
+    section.enrolledStudents.push(parseInt(studentId));
+    return await saveDataToApi('/sections', section);
+  }
+  throw new Error('Section not found');
+  
+  
+}
+
 async function updateSection(sectionId, data) {
   const updatedSection = {
     id: sectionId,
@@ -68,5 +79,6 @@ export {
   fetchSectionById, 
   fetchSectionsByCourseId,
   fetchSectionsBySemester,
-  deleteSectionById 
+  deleteSectionById,
+  addStudentToSection
 }; 
